7c06553c5fa3ba3de0a6cc1351d0d16a7a130bbd
Daniel Tse <danieltse@gmail.com>
2014-11-13 22:51:01 +1100

    Initial Commit

---
+++ Taskmate_alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taskmate_alpha
+//
+//  Created by Daniel Tse on 13/11/2014.
+//  Copyright (c) 2014 Daniel Tse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
