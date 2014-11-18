7d9096b45d689bf68af4b0ed76233de2e7e914da
Daniel Tse <danieltse@gmail.com>
2014-11-19 01:50:41 +1100

    Getting current user location using Apple map

--- Taskmate_alpha/MapViewController.swift
+++ Taskmate_alpha/MapViewController.swift
@@ -9,54 +9,91 @@
 import UIKit
 import MapKit
 
-
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var mView: CustomView!
     
     @IBOutlet var appleMap: MKMapView!
     
+    var locManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.868,
-            longitude:151.2086, zoom:16)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
-        mapView.settings.myLocationButton = true
-        mapView.settings.zoomGestures = true
-        mapView.myLocationEnabled = true
-        mapView.accessibilityElementsHidden = false
-        
-        
-        var marker = GMSMarker()
-        marker.position = camera.target
-        marker.snippet = "Hello World"
-        marker.appearAnimation = kGMSMarkerAnimationPop
-        marker.draggable = true
-        marker.map = mapView
+//        var camera = GMSCameraPosition.cameraWithLatitude(-33.868,
+//            longitude:151.2086, zoom:16)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+//        mapView.settings.myLocationButton = true
+//        mapView.settings.zoomGestures = true
+//        mapView.myLocationEnabled = true
+//        mapView.accessibilityElementsHidden = false
+//
+//        var marker = GMSMarker()
+//        marker.position = camera.target
+//        marker.snippet = "Hello World"
+//        marker.appearAnimation = kGMSMarkerAnimationPop
+//        marker.draggable = true
+//        marker.map = mapView
 //        self.view = mapView
         
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        locManager.requestWhenInUseAuthorization()
+        locManager.requestAlwaysAuthorization()
+        locManager.startUpdatingLocation()
         
-        let location = CLLocationCoordinate2D(
-            latitude: 51.50007773,
-            longitude: -0.1246402
-        )
+        appleMap.showsUserLocation = true
+        appleMap.showsPointsOfInterest = true
+
+        //        let location = CLLocationCoordinate2D(
+        //            latitude: 51.50007773,
+        //            longitude: -0.1246402
+        //        )
+
         
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: appleMap.userLocation.coordinate,
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        locManager.startUpdatingLocation()
+
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let span = MKCoordinateSpanMake(0, 0)
+        let region = MKCoordinateRegion(center: locManager.location.coordinate,
             span: span)
-        
         appleMap.setRegion(region, animated: true)
-        
+
         let annotation = MKPointAnnotation()
-        annotation.setCoordinate(location)
+        annotation.setCoordinate(locManager.location.coordinate)
         annotation.title = "Big Ben"
         annotation.subtitle = "London"
         
-        appleMap.addAnnotation(annotation)
+        let pinView = MKPinAnnotationView()
+//        pinView.pinColor = MKPinAnnotationColorRed;
+        pinView.draggable = true
+        pinView.annotation = annotation
         
+
         
+                appleMap.addAnnotation(annotation)
+
+
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: userLocation.coordinate,
+            span: span)
+        appleMap.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        println("checking...");
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            appleMap.showsUserLocation = true
+        }
     }
     
     
